c7d4fafc1647998476756a00c61a546715d7a107
MariaYablokova <MaOYablokova@yandex.ru>
2024-09-27 01:22:23 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -8,7 +8,7 @@ for i in numbers:
     for j in range(2, i):
         if i % j == 0:
            is_prime = False
-        break
+           break
     if is_prime:
         primes.append(i)
     else:
